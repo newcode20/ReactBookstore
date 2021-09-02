8b938ee013c39cbc8e198bb44dcc7bd44ced4bb7
Kimberly Wooten <kwooten1993@gmail.com>
2021-09-01 23:37:56 -0400

    props

--- src/index.js
+++ src/index.js
@@ -16,32 +16,41 @@ import './index.css'
 //   return <h4>This is Kimberly and this is my first component</h4>;
 // }
 
+//set up vars
+const firstBook = {
+author: 'Rupi Kaur',
+title:'milk and honey',
+img: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+};
+
+const secondBook = {
+author: 'Colleen Hoover',
+title:'Verity',
+img: "https://images-na.ssl-images-amazon.com/images/I/41N1GA7CaNS._SX322_BO1,204,203,200_.jpg"
+};
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+{/* want name prop or pass it in where the component is being rendered, but you are accessing it in the component first  */}
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
     </section> 
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  
   return (
       <article className="book">
-        <Image></Image>
-        <Title />
-        <Author />
+        <img src={props.img} alt="milk and honey book" />
+        <h1> {props.title} </h1>
+        <h4>{props.author.toUpperCase()} </h4>
+        
       </article>
   );
 }
 
-const Title = () => <h1> milk and honey </h1>;
-const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop: '.25rem'}}> Rupi Kaur</h4>
-
-const Image = () => <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="milk and honey book" />
 
 //Creating a component with using no JSX and creating an element, not as easy.
 // const Greeting = () => {
